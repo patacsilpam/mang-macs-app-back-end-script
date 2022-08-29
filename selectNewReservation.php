799eb00f249bcc7af0301332c263fe3ee067dcd5
Pam Patacsil <patacsilpamela.bsit@gmail.com>
2022-08-29 20:25:09 +0800

    initial commit

--- selectNewReservation.php
+++ selectNewReservation.php
@@ -3,11 +3,32 @@ require 'database/connection.php';
 date_default_timezone_set("Asia/Manila");
 $time = date('h:i a');
 $emailAddress = mysqli_real_escape_string($connect,$_GET['emailAddress']);
-$selectNewOrders = $connect->query("SELECT * FROM tblreservation WHERE scheduled_date >= CURDATE() AND status != 'Cancelled' HAVING email='$emailAddress'");
+$selectNewOrders = $connect->query("SELECT tblreservation.id,tblreservation.refNumber,tblreservation.fname,tblreservation.lname,tblreservation.guests,
+tblreservation.email,tblreservation.created_at,DATE_FORMAT(tblreservation.scheduled_date,'%a,  %b %d, %Y') as 'scheduled_date',tblreservation.scheduled_time,
+tblreservation.status,tblreservation.totalAmount,tblreservation.payment_photo,
+tblorderdetails.product_code,tblorderdetails.product_name,tblorderdetails.product_category,
+tblorderdetails.product_variation,tblorderdetails.quantity,tblorderdetails.price,tblorderdetails.add_ons,
+tblorderdetails.product_image,tblorderdetails.completed_time 
+FROM tblreservation LEFT JOIN tblorderdetails
+ON tblreservation.refNumber = tblorderdetails.order_number
+WHERE STR_TO_DATE(CONCAT(scheduled_date,' ', scheduled_time),'%Y-%m-%d %h:%i %p') >= DATE_SUB(CURDATE(), INTERVAL 30 MINUTE) 
+AND tblreservation.status != 'Cancelled'
+HAVING tblreservation.email='$emailAddress'");
 $response = array();
 while($fetch = $selectNewOrders->fetch_assoc()){
     $response[] = $fetch;
 
 }
 echo json_encode($response);
+/*
+SELECT tblreservation.id,tblreservation.fname,tblreservation.lname,tblreservation.guests,
+tblreservation.email,tblreservation.created_at,tblreservation.scheduled_date,tblreservation.scheduled_time,
+tblreservation.status,tblreservation.totalAmount,tblreservation.payment_photo,
+tblorderdetails.product_code,tblorderdetails.product_name,tblorderdetails.product_category,
+tblorderdetails.product_variation,tblorderdetails.quantity,tblorderdetails.price,tblorderdetails.add_ons,
+tblorderdetails.product_image,tblorderdetails.completed_time 
+FROM tblreservation LEFT JOIN tblorderdetails
+ON tblreservation.refNumber = tblorderdetails.order_number
+WHERE STR_TO_DATE(CONCAT(scheduled_date,' ', scheduled_time),'%Y-%m-%d %h:%i %p') >= DATE_SUB(CURDATE(), INTERVAL 30 MINUTE);
+*/
 ?>
